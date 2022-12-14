ed9963c097709a570b4a75aad2ebb05c814e18bc
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-13 21:52:14 -0300

    Create notification entity

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,39 @@
+import { randomUUID } from 'crypto';
+import { Replace } from '../../helpers/replace';
+import { Content } from '../validators/content';
+
+interface INotification {
+  content: Content;
+  category: string;
+  readtAt?: Date | null;
+  createdAt: Date;
+  recipientId: string;
+}
+
+export class Notification {
+  id: string;
+  content: Content;
+  category: string;
+  readtAt?: Date | null;
+  createdAt: Date;
+  recipientId: string;
+
+  constructor({
+    content,
+    category,
+    readtAt,
+    createdAt = new Date(),
+    recipientId,
+  }: Replace<INotification, { createdAt?: Date }>) {
+    this.id = randomUUID();
+    this.content = content;
+    this.category = category;
+
+    if (readtAt) {
+      this.readtAt = readtAt;
+    }
+
+    this.createdAt = createdAt;
+    this.recipientId = recipientId;
+  }
+}

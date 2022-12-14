6207210525e281bb025892f7746761d325efa8d2
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-13 21:54:36 -0300

    Create notification prisma repository

---
+++ src/infra/database/prisma/repositories/notification.ts
@@ -0,0 +1,22 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationRepository } from '../../../../application/repositories/notification';
+import { PrismaService } from '../prisma.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrismaNotificationRepository implements NotificationRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readtAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}

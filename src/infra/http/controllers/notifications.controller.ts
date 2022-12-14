e0aba02361915323cd9b80cba32ba32d40268e6a
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-13 21:54:20 -0300

    Create notification controller

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { CreateNotification } from '../dtos/notification.dto';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotification) {
+    const { content, category, recipientId } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      content,
+      category,
+      recipientId,
+    });
+
+    return { notification };
+  }
+}

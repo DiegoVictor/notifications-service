5420deed5e9086885fa83a4257b7efc0633c0f30
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-15 18:08:44 -0300

    Update in memory repository

--- tests/repositories/notification.ts
+++ tests/repositories/notification.ts
@@ -4,7 +4,35 @@ import { NotificationRepository } from '@application/repositories/notification';
 export class InMemoryNotificationRepository implements NotificationRepository {
   public notifications: Notification[] = [];
 
-  async create(notification: Notification) {
+  constructor(notifications?: Notification[]) {
+    if (notifications) {
+      this.notifications = notifications;
+    }
+  }
+
+  public async findOneById(id: string): Promise<Notification> {
+    return this.notifications.find((notification) => notification.id === id);
+  }
+
+  public async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
+  public async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  public async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      ({ id }) => notification.id === id,
+    );
+
+    if (notificationIndex === -1) {
+      return null;
+    }
+
+    this.notifications.splice(notificationIndex, 1, notification);
   }
 }

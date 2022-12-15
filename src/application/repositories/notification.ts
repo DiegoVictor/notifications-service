d409e0d05ff0135e1a5599dd15aa57df2efca7c2
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-15 18:09:26 -0300

    Update repository interface

--- src/application/repositories/notification.ts
+++ src/application/repositories/notification.ts
@@ -2,4 +2,7 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationRepository {
   public abstract create(notification: Notification): Promise<void>;
+  public abstract findOneById(id: string): Promise<Notification | null>;
+  public abstract save(notification: Notification): Promise<void>;
+  public abstract countManyByRecipientId(recipientId: string): Promise<number>;
 }

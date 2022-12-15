cf0b3840ddfe0de816f107a9384a86176dc9fd5f
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-15 18:38:00 -0300

    Create use case to read a notification

---
+++ src/application/use-cases/read-notification.ts
@@ -0,0 +1,29 @@
+import { Notification } from '../entities/notification';
+import { NotificationRepository } from '../repositories/notification';
+import { Content } from '../validators/content';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFound } from '@application/errors/notification';
+
+interface IRequest {
+  notificationId: string;
+}
+
+@Injectable()
+export class ReadNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(request: IRequest): Promise<void> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findOneById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+    notification.read();
+
+    await this.notificationRepository.save(notification);
+  }
+}

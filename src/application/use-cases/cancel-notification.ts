86666b03e1b0eb4c7b727ff7e2939d6880b47947
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-15 18:10:00 -0300

    Create cancel notification service

---
+++ src/application/use-cases/cancel-notification.ts
@@ -0,0 +1,27 @@
+import { NotificationRepository } from '../repositories/notification';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFound } from '@application/errors/notification';
+
+interface IRequest {
+  notificationId: string;
+}
+
+@Injectable()
+export class CancelNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(request: IRequest): Promise<void> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findOneById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+    notification.cancel();
+
+    await this.notificationRepository.save(notification);
+  }
+}

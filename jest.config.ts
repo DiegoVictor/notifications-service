c604fc4f23768d0a1fcfb0e76b8139ddd6c2511e
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-17 18:20:12 -0300

    Update jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
   coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['text', 'lcov', 'text-summary'],
+  coverageReporters: ['text', 'json', 'lcov', 'text-summary'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,

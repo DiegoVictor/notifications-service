1a1a8af51472b177b8b9561da7154941beb2b82b
Diego Victor <diegovictorgonzaga@gmail.com>
2022-12-19 11:40:41 -0300

    Create consumer

---
+++ src/infra/messaging/kafka/consumer.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}

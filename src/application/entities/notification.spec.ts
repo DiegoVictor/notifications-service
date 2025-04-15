9f9e078e6fb817ebd97bed9885f0e494aad486d7
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-04-14 22:20:02 -0300

    Remove not needed async clause

--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -3,13 +3,13 @@ import { makeNotification } from '@tests/factories/notification';
 import { Notification } from './notification';
 
 describe('Notification', () => {
-  it('should be able to create a notification', async () => {
+  it('should be able to create a notification', () => {
     const notification = makeNotification({ readAt: new Date() });
 
     expect(notification).toBeInstanceOf(Notification);
   });
 
-  it('should be able to cancel a notification', async () => {
+  it('should be able to cancel a notification', () => {
     const notification = makeNotification();
 
     expect(notification.canceledAt).toBe(undefined);
@@ -19,7 +19,7 @@ describe('Notification', () => {
     expect(notification.canceledAt).toStrictEqual(expect.any(Date));
   });
 
-  it('should be able to read a notification', async () => {
+  it('should be able to read a notification', () => {
     const notification = makeNotification();
 
     expect(notification.readAt).toBe(undefined);
@@ -29,7 +29,7 @@ describe('Notification', () => {
     expect(notification.readAt).toStrictEqual(expect.any(Date));
   });
 
-  it('should be able to unread a notification', async () => {
+  it('should be able to unread a notification', () => {
     const readAt = faker.date.past();
     const notification = makeNotification({
       readAt,

201844ebeb47becc83132475697b365c0203e53b
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-04-17 09:01:00 -0300

    Comment out coverageProvider in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
   ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  // coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text', 'json', 'lcov', 'text-summary'],
